225793907faef481f0aeff7f2551e99ddcb1598d
vancodocton <duongtruong.nguyenky@gmail.com>
2021-02-25 22:09:21 +0700

    remove unnecessary code and change redirect page

--- auth/checklogin.php
+++ auth/checklogin.php
@@ -1,17 +1,9 @@
 <?php
-    if (session_id() === '')
-    {
-        session_start();
-    }    
     if (!isset($_SESSION['account_id']))
     {
         $_SESSION['originURL'] = $_SERVER['PHP_SELF'];
         $url = 'Location: /auth/login.php';
         header($url);
         exit();
-    }
-    else
-    {
-        echo $_SESSION['account_id'];
     }
 ?>
