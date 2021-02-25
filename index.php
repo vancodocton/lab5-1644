e0042dbbe0e00ec8ce7cf6ed92919c5bec586681
vancodocton <duongtruong.nguyenky@gmail.com>
2021-02-25 22:09:57 +0700

    remove unnecessary and add redirect

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
+	echo $_SERVER['PHP_SELF'];
     if (session_id() === '')
-    {
         session_start();
-    }
 	require './auth/checklogin.php';
-	// if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) 
-	// {
-	// 	$uri = 'https://';
-	// } else 
-	// {	
-	// 	$uri = 'http://';
-	// }
-	// $uri .= $_SERVER['HTTP_HOST'];
-	// header('Location: ' . $uri . '/sample-sidebar/');
-	echo '<p>'.$_SERVER['PHP_SELF'].'</p><br>';
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
+	switch ($_SESSION['account_roleid'])
+    {
+        case 1:
+            $url = 'Location: /com/';
+            break;
+        case 2:
+            $url = 'Location: /store/';
+            break;
+        default:
+            require $_SERVER['DOCUMENT_ROOT'] . '/permissiondeny.html';
+            exit();
+    }
+	header($url);
+	exit();
 ?>
 <p>ATN Company</p>
 <button type="button" class="btn btn-primary" onclick="location.href ='./auth/logout.php'">logout</button>
