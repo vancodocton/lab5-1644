6d19347e565422fdc80f1a2317ade867b997e7ed
vancodocton <duongtruong.nguyenky@gmail.com>
2021-02-25 21:08:08 +0700

    change localhost database credential

--- backend/dbConnect.php
+++ backend/dbConnect.php
@@ -1,9 +1,9 @@
 <?php
-    $hostname = "ec2-54-157-149-88.compute-1.amazonaws.com";
+    $hostname = "127.0.0.1";
     $port = 5432;
-    $dbName = "damkvqjigb97iu";
-    $username = "elildunpndkouv";
-    $password = "124fc1e3fef5429577a8361bc7b68b50a978c5f8cbe3d356e282448dee796863";
+    $dbName = "postgres";
+    $username = "postgres";
+    $password = "asdasdas";
     $connectionString = "host=$hostname port=$port dbname=$dbName user=$username password=$password";
 
     $dbServer = pg_connect($connectionString);
