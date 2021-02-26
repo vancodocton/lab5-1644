c35ba202a6aaaf6e211d4b29c0899537c4013170
vancodocton <duongtruong.nguyenky@gmail.com>
2021-02-26 12:13:55 +0700

    add session time out function

--- auth/checklogin.php
+++ auth/checklogin.php
@@ -1,9 +1,21 @@
 <?php
-    if (!isset($_SESSION['account_id']))
-    {
-        $_SESSION['originURL'] = $_SERVER['PHP_SELF'];
-        $url = 'Location: /auth/login.php';
-        header($url);
-        exit();
-    }
+	if (!isset($_SESSION['account_id']))
+	{
+		$_SESSION['originURL'] = $_SERVER['PHP_SELF'];
+		$url = 'Location: /auth/login.php';
+		header($url);
+		exit();
+	}
+	else
+	{
+		if (isset( $_SESSION['last_request_time']))
+		{
+			$timelive = $_SERVER['REQUEST_TIME'] - $_SESSION['login_time_stamp'];
+			if ($timelive > 60*60)
+			{
+				session_destroy();
+			}
+		}
+		$_SESSION['last_request_time'] = $_SESSION['login_time_stamp'];
+	}
 ?>
