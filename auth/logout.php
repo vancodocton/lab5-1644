7bc7a0b4514ac282b4bdd11f8d42210223ca7b7d
vancodocton <duongtruong.nguyenky@gmail.com>
2021-02-26 12:15:09 +0700

    modify to detroy all session variables

--- auth/logout.php
+++ auth/logout.php
@@ -4,9 +4,11 @@
 
     if (isset($_SESSION['account_id']))
     {
-        unset($_SESSION['account_id']);
-        unset($_SESSION['account_username']);
-        unset($_SESSION['account_roleid']);
+        // unset($_SESSION['account_id']);
+        // unset($_SESSION['account_username']);
+        // unset($_SESSION['account_roleid']);
+        // unset($_SESSION['last_request_time']);
+        session_destroy();
         header('Location: /auth/login.php');
     }   
     exit();
